af5b1cf3a7673d7bde36fc1ac2be35d934e0b71c
Eduardo Sene <eduardosenebarbosa@hotmail.com>
2022-10-26 21:54:24 -0300

    inciando projeto

---
+++ clone-investir-em-que-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ieq-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'clone-investir-em-que-angular';
+}
